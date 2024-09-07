37fc9aec218464a4e66de0ee1572cface4e0a680
stephan ullmann <stephan.ullmann@posteo.de>
2024-09-07 14:46:48 +0200

    websockets

--- src/game.ts
+++ src/game.ts
@@ -1,16 +1,21 @@
 import { checkIsMill, connectedMap, lToi } from './utils';
+let socket: WebSocket | null;
 let boardEl: HTMLDivElement;
 let playerIndicator: HTMLHeadingElement;
 let playPiece: HTMLDivElement;
 let millEl: HTMLDivElement;
+
+let playerBlackName = 'Black';
+let playerWhiteName = 'White';
+let playerSelf: 'black' | 'white' = 'black';
 
 let isPlayerOne = true;
 
 let phaseOneRound = 1;
 let isPhaseTwo = false;
 
-let p1Places: string[] = [];
-let p2Places: string[] = [];
+let p1Points: string[] = [];
+let p2Points: string[] = [];
 
 let isMill = false;
 
@@ -21,16 +26,17 @@ let selectedPoint: HTMLDivElement | null;
 let millStateRunning = false;
 
 function endGame() {
-  boardEl.inert = true;
+  // boardEl.inert = true;
   document.getElementById('reset')!.classList.remove('hidden');
   boardEl.removeEventListener('click', handleBoardClick);
   document.removeEventListener('mousemove', handleMouseMove);
-  playerIndicator.textContent = isPlayerOne ? 'Player One Won!' : 'Player Two Won!';
+  const text = isPlayerOne ? playerBlackName : playerWhiteName;
+  playerIndicator.textContent = text + ' won!';
 }
 
 function isInMill(piece: HTMLDivElement) {
   const place = piece.style.getPropertyValue('--place');
-  const opponentPlaces = isPlayerOne ? [...p2Places] : [...p1Places];
+  const opponentPlaces = isPlayerOne ? [...p2Points] : [...p1Points];
   opponentPlaces.splice(
     opponentPlaces.findIndex((p) => p === place),
     1
@@ -56,13 +62,13 @@ function handleMillClick(point: HTMLDivElement) {
   const clickedPlace = point.style.getPropertyValue('--place');
   if (!clickedPlace || !point.classList.contains('clickablePiece')) return;
   if (isPlayerOne) {
-    p2Places.splice(
-      p2Places.findIndex((p) => clickedPlace === p),
+    p2Points.splice(
+      p2Points.findIndex((p) => clickedPlace === p),
       1
     );
   } else {
-    p1Places.splice(
-      p1Places.findIndex((p) => clickedPlace === p),
+    p1Points.splice(
+      p1Points.findIndex((p) => clickedPlace === p),
       1
     );
   }
@@ -77,16 +83,22 @@ function endMillState() {
   millStateRunning = false;
   opponentPieces.forEach((piece) => piece.classList.remove('clickablePiece'));
   if (isPhaseTwo) {
-    const opponentPlaces = isPlayerOne ? p2Places : p1Places;
+    const opponentPlaces = isPlayerOne ? p2Points : p1Points;
     if (opponentPlaces.length < 3) endGame();
   }
 }
 
 function endRound() {
   if (selectedPoint) return;
-  if (boardEl.inert) return;
+  // if (boardEl.inert) return;
+  if (socket && (playerSelf === 'black') === isPlayerOne) {
+    boardEl.inert = true;
+  } else {
+    boardEl.inert = false;
+  }
   isPlayerOne = !isPlayerOne;
-  playerIndicator.textContent = isPlayerOne ? 'Player One' : 'Player Two';
+  const text = isPlayerOne ? playerBlackName : playerWhiteName;
+  playerIndicator.textContent = text + "'s turn";
   document.body.classList.toggle('player-two');
 }
 
@@ -98,7 +110,7 @@ function handleMove(point: HTMLDivElement) {
   }
   const place = point.style.getPropertyValue('--place');
 
-  const playerPlaces = isPlayerOne ? p1Places : p2Places;
+  const playerPlaces = isPlayerOne ? p1Points : p2Points;
   if (playerPlaces.includes(place)) {
     selectedPoint?.classList.remove('selected');
     selectedPoint = point;
@@ -108,7 +120,7 @@ function handleMove(point: HTMLDivElement) {
 
   const oldPlace = selectedPoint?.style.getPropertyValue('--place') as keyof typeof connectedMap;
   if (!connectedMap[oldPlace].includes(place) && playerPlaces.length > 3) return;
-  const opponentPlaces = isPlayerOne ? p2Places : p1Places;
+  const opponentPlaces = isPlayerOne ? p2Points : p1Points;
   if (opponentPlaces.includes(place)) return;
 
   const yMove = Number(oldPlace[1]) - Number(place[1]);
@@ -118,13 +130,13 @@ function handleMove(point: HTMLDivElement) {
   selectedPoint!.style.transform = `translate(calc(min(10.5cqh, 10.5cqw) * ${xMove}), calc(min(10.5cqh, 10.5cqw) * ${yMove}))`;
   setTimeout(() => {
     if (isPlayerOne) {
-      p1Places.splice(
-        p1Places.findIndex((p) => oldPlace === p),
+      p1Points.splice(
+        p1Points.findIndex((p) => oldPlace === p),
         1
       );
     } else {
-      p2Places.splice(
-        p2Places.findIndex((p) => oldPlace === p),
+      p2Points.splice(
+        p2Points.findIndex((p) => oldPlace === p),
         1
       );
     }
@@ -148,9 +160,9 @@ function selectPoint(point: HTMLDivElement) {
 
 function handlePlacement(point: HTMLDivElement) {
   const place = point.style.getPropertyValue('--place');
-  const placesArr = isPlayerOne ? p1Places : p2Places;
+  const placesArr = isPlayerOne ? p1Points : p2Points;
   isMill = checkIsMill(place, placesArr);
-  isPlayerOne ? p1Places.push(place) : p2Places.push(place);
+  isPlayerOne ? p1Points.push(place) : p2Points.push(place);
   point.classList.add(isPlayerOne ? 'p1' : 'p2');
   if (!isPhaseTwo && !isPlayerOne) phaseOneRound++;
 
@@ -167,9 +179,13 @@ function handlePlacement(point: HTMLDivElement) {
   }
 }
 
-function handleBoardClick(e: MouseEvent) {
+export function handleBoardClick(e: MouseEvent) {
   const point = (e.target as HTMLDivElement).closest('.point') as HTMLDivElement;
   if (!point) return;
+  // if socket and it's my turn
+  if (socket && (playerSelf === 'black') === isPlayerOne) {
+    socket.send(playerSelf + '-' + point.id);
+  }
   if (isMill) handleMillClick(point);
   else if (selectedPoint) {
     handleMove(point);
@@ -194,33 +210,41 @@ function handleMouseMove(e: MouseEvent) {
 }
 
 function startGame() {
+  // console.log(socket);
+  boardEl = document.querySelector<HTMLDivElement>('.board')!;
+
+  if (socket && (playerSelf === 'white') === isPlayerOne) {
+    boardEl.inert = true;
+  } else {
+    boardEl.inert = false;
+  }
+
   document.getElementById('reset')!.classList.add('hidden');
   document.body.classList.remove('phase-two', 'player-two');
   isPlayerOne = true;
   phaseOneRound = 1;
-  p1Places = [];
-  p2Places = [];
+  p1Points = [];
+  p2Points = [];
   isPhaseTwo = false;
   isMill = false;
   millStateRunning = false;
   opponentPieces = document.querySelectorAll('not-in-doc');
   selectedPoint = null;
-  boardEl = document.querySelector<HTMLDivElement>('.board')!;
-  boardEl.inert = false;
   playerIndicator = document.getElementById('player-indicator')! as HTMLHeadingElement;
   playPiece = document.getElementById('play-piece')! as HTMLDivElement;
   millEl = document.querySelector('.mill')! as HTMLDivElement;
 
   millEl.classList.add('hidden');
 
-  playerIndicator.textContent = 'Player One';
+  playerIndicator.textContent = playerBlackName + "'s turn";
 
   [...boardEl.children].forEach((el) => {
     el.classList.remove('p1', 'p2');
   });
 
   ['p1', 'p2'].forEach((player) => {
-    const container = document.getElementById(`pieces-${player}`);
+    const container = document.getElementById(`pieces-${player}`)!;
+    container.innerHTML = '';
     const ind = `<div class="${player}ind"></div>`;
     Array.from(Array(9)).forEach((_) => container?.insertAdjacentHTML('beforeend', ind));
   });
@@ -229,7 +253,16 @@ function startGame() {
   document.addEventListener('mousemove', handleMouseMove);
 }
 
-export default function game() {
-  window.addEventListener('load', startGame);
+export default function game(
+  socketParam: WebSocket | null,
+  options?: { playerBlack: string; playerWhite: string; playerSelf: 'black' | 'white' }
+) {
+  socket = socketParam;
+  if (socket) {
+    playerBlackName = options!.playerBlack;
+    playerWhiteName = options!.playerWhite;
+    playerSelf = options!.playerSelf;
+  }
+  startGame();
   document.getElementById('reset')!.addEventListener('click', startGame);
 }
